efa448622b17278221f95b6cc89bceedc622e6b9
a1993azad <a1993azad@gmail.com>
2023-10-16 16:55:46 +0330

    hourly mark volume box was implemented and ChartCarousel was created to handle slide effect for bar chart

--- src/components/HourlyMarketVolume.js
+++ src/components/HourlyMarketVolume.js
@@ -1,39 +1,63 @@
-import React from 'react'
-import { getHourlyExchangeVolume } from '../services/cryptoEndpoints';
-import FetchWithCache from '../utils/FetchWithCache';
+import React from "react";
+import { getHourlyExchangeVolume } from "../services/cryptoEndpoints";
+import useAPI from "../hooks/useAPI";
+import { Card } from "react-bootstrap";
+import Loading from "./Loading";
+import labels from "../constants/labels";
+import formatTime from "../utils/formatTime";
+import "../assets/scss/hourlyMarketVolume.scss";
+import ChartCarousel from "./ChartCarousel";
 
-function HourlyMarketVolume({cryptocurrencySymbol}) {
-    const [loading, setLoading] = React.useState(false);
-    const [hourlyPriceData, setHourlyPriceData] = React.useState(null);
-    const apiRef = React.useRef();
-    const getHourlyMarketVolume = async () => {
-      try {
-        setLoading(true);
-        const res = await apiRef.current.fetchData();
-      } catch (error) {
-        console.error(error);
-      } finally {
-        setLoading(false);
+function HourlyMarketVolume({ cryptocurrencySymbol }) {
+  const [hourlyMarketVolumes, setHourlyMarketVolumes] = React.useState(null);
+  const [loading, fetchAPI, setFetchParams] = useAPI();
+
+  const getHourlyMarketVolume = async () => {
+    try {
+      setFetchParams(getHourlyExchangeVolume({ cryptocurrencySymbol }));
+      const res = await fetchAPI();
+      if (Array.isArray(res?.Data)) {
+        const chartData = res.Data.map((data) => {
+          return {
+            time: formatTime(data.time),
+            volume: data.volume.toFixed(2),
+          };
+        });
+        setHourlyMarketVolumes(chartData);
       }
-    };
-    const initAPI = () => {
-      apiRef.current = new FetchWithCache(
-        getHourlyExchangeVolume({ cryptocurrencySymbol })
-      );
-    };
-  
-    React.useEffect(() => {
-      initAPI();
-      getHourlyMarketVolume();
-      return () => {
-        if (apiRef.current && "cancelFetch" in apiRef.current) {
-          apiRef.current.cancelFetch();
-        }
-      };
-    }, [cryptocurrencySymbol]);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const getTitle = (activeVolumeIndex) =>
+    hourlyMarketVolumes?.length && activeVolumeIndex >= 0
+      ? labels.marketVolumeOf(hourlyMarketVolumes[activeVolumeIndex]?.time)
+      : "";
+
+  React.useEffect(() => {
+    getHourlyMarketVolume();
+  }, [cryptocurrencySymbol]);
+
   return (
-    <div>HourlyMarketVolume</div>
-  )
+    <Card className=" rounded-4">
+      <Card.Body>
+        {loading ? (
+          <Loading height={300} />
+        ) : (
+          <>
+            <ChartCarousel
+              data={hourlyMarketVolumes}
+              color="green"
+              getTitle={getTitle}
+              xAxisDataKey="time"
+              yAxisDataKey="volume"
+            />
+          </>
+        )}
+      </Card.Body>
+    </Card>
+  );
 }
 
-export default HourlyMarketVolume
+export default HourlyMarketVolume;
